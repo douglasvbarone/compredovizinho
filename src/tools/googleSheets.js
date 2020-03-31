e9601b302001ca25aa33025375e74062e90aaa1b
Douglas Barone <douglasvbarone@gmail.com>
2020-03-31 09:01:52 -0400

    Sheet change

--- src/tools/googleSheets.js
+++ src/tools/googleSheets.js
@@ -1,19 +1,19 @@
 const parseSheets = raw =>
   raw.feed.entry.map(entry => ({
-    empresa: entry.gsx$empresa.$t || null,
+    empresa: entry.gsx$empresa.$t,
     whatsapp:
-      (entry.gsx$whatappdddsemozero.$t &&
-        entry.gsx$whatappdddsemozero.$t.replace(/[^\d]/g, '')) ||
-      null,
-    cnpj: entry.gsx$cnpj.$t || null,
-    email: entry['gsx$e-mail'].$t || null,
-    segmento: entry.gsx$segmento.$t || null,
-    rua: entry.gsx$rua.$t || null,
-    numero: entry['gsx$número'].$t || null,
-    comp: entry['gsx$comp.'] || null,
-    bairro: entry.gsx$bairro.$t || null,
-    cidade: entry.gsx$cidade.$t || null,
-    cep: entry.gsx$cep.$t || null
+      entry.gsx$whatsappdddsemozeroapenasnúmeros.$t &&
+      entry.gsx$whatsappdddsemozeroapenasnúmeros.$t.replace(/[^\d]/g, ''),
+    cpf: entry.gsx$cnpjoucpf.$t,
+    email: entry['gsx$e-mail'].$t,
+    segmento: entry.gsx$segmento.$t,
+    rua: entry.gsx$rua.$t,
+    numero: entry['gsx$número'].$t,
+    comp: entry['gsx$comp.'].$t,
+    bairro: entry.gsx$bairro.$t,
+    cidade: entry.gsx$cidade.$t,
+    cep: entry.gsx$cep.$t,
+    updatedAt: entry.gsx$carimbodedatahora.$t
   }))
 
 export { parseSheets }
